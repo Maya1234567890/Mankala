685223f6dcf61be394766e9664f22965aa4b8f77
Maya1234567890 <vaksin.maya@gmail.com>
2021-11-06 14:33:44 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,12 @@
-print("hello")
-print("ok")
+from socket import*
+import sys
+
+HOST = '109.66.29.58'
+PORT = '45000'
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+tcpcliesock = socket(AF_INET, SOCK_STREAM)
+tcpcliesock.settimeout(5)
+
+tcpcliesock.connect(ADDR)
+
