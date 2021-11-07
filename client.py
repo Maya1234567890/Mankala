16bf8a2e101aab4aef0d1712daf4d8637c86144a
Maya1234567890 <vaksin.maya@gmail.com>
2021-11-07 22:49:26 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,32 @@
 from socket import*
 import sys
+import json
 
-HOST = '109.66.29.58'
-PORT = '45000'
+HOST = '79.179.71.212'
+PORT = 45000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 tcpcliesock = socket(AF_INET, SOCK_STREAM)
-tcpcliesock.settimeout(5)
+#tcpcliesock.settimeout(5)
 
-tcpcliesock.connect(ADDR)
+try:  # try to connect to the server's socket.
+    tcpcliesock.connect(ADDR)
+except ConnectionRefusedError:
+    print("The server is closed")
+except timeout:
+    print("Time ran out. Please try again later")
+else:
+    while 1:
+        data = tcpcliesock.recv(BUFSIZ)
+        print(data.decode('utf-8'))
+        data = {}
+        while 1:
+            key = input(">>")
+            if key == "": exit()
+            value = input(">>")
+            if value == "": value = True
+            data[key] = value
+        tcpcliesock.send(json.dumps(data).encode('utf-8'))
+
+tcpcliesock.close()
 
