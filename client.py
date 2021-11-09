e6125fc79a8ec53dee8222df5ae666e341065a40
Maya1234567890 <vaksin.maya@gmail.com>
2021-11-09 16:31:56 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,21 @@
-from socket import*
+from socket import *
 import sys
 import json
+from threading import Thread
+
+
+def server_recv(socket):
+    while 1:
+        data = socket.recv(1024)
+        print(data)
+
 
 HOST = '79.179.71.212'
 PORT = 45000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 tcpcliesock = socket(AF_INET, SOCK_STREAM)
-#tcpcliesock.settimeout(5)
+# tcpcliesock.settimeout(5)
 
 try:  # try to connect to the server's socket.
     tcpcliesock.connect(ADDR)
@@ -16,11 +24,11 @@ except ConnectionRefusedError:
 except timeout:
     print("Time ran out. Please try again later")
 else:
+    server_recv_thread = Thread(target=lambda: server_recv(tcpcliesock), daemon=True)
+    server_recv_thread.start()
+    #Thread.start(server_recv(tcpcliesock))
+    data = {}
     while 1:
-        data = tcpcliesock.recv(BUFSIZ)
-        # another recv for game started.
-        print(data.decode('utf-8'))
-        data = {}
         while 1:
             key = input(">>")
             if key == "": break
@@ -30,4 +38,3 @@ else:
         tcpcliesock.send(json.dumps(data).encode('utf-8'))
 
 tcpcliesock.close()
-
