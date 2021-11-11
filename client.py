175819b7f4b0759621db93054c30dfc467bbe3bb
Mike Shlapakov <mike.shlapakov@gmail.com>
2021-11-12 00:06:04 +0200

    Basic login window

--- client.py
+++ client.py
@@ -2,39 +2,79 @@ from socket import *
 import sys
 import json
 from threading import Thread
+import loginUI
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 def server_recv(socket):
     while 1:
-        data = socket.recv(1024)
+        data = json.loads(socket.recv(BUFSIZ).decode()[5:])
         print(data)
 
 
-HOST = '79.179.71.212'
+def start_game(name):
+    if not name.isalnum():
+        print(name)
+        return
+    tcpcliesock.send(json.dumps({"type":"Login","name":name}).encode('utf-8'))
+    data = json.loads(tcpcliesock.recv(BUFSIZ).decode()[5:])
+    print(data)
+    if data["type"] != "Error":
+        tcpcliesock.send(json.dumps({"type": "Start Game"}).encode('utf-8'))
+        data = json.loads(tcpcliesock.recv(BUFSIZ).decode()[5:])
+    print(data)
+
+
+def join_game(name, id):
+    if not name.isalnum() or not id.isdigit():
+        print(name, id)
+        return
+    tcpcliesock.send(json.dumps({"type":"Login","name":name}).encode('utf-8'))
+    data = json.loads(tcpcliesock.recv(BUFSIZ).decode()[5:])
+    print(data)
+    if data["type"] != "Error":
+        tcpcliesock.send(json.dumps({"type": "Join Game", "game_id":eval(id)}).encode('utf-8'))
+        data = json.loads(tcpcliesock.recv(BUFSIZ).decode()[5:])
+    print(data)
+
+
+HOST = '109.65.31.250'
 PORT = 45000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 tcpcliesock = socket(AF_INET, SOCK_STREAM)
 # tcpcliesock.settimeout(5)
+app = QtWidgets.QApplication(sys.argv)
 
 try:  # try to connect to the server's socket.
     tcpcliesock.connect(ADDR)
 except ConnectionRefusedError:
     print("The server is closed")
-except timeout:
+except TimeoutError:
     print("Time ran out. Please try again later")
-else:
-    server_recv_thread = Thread(target=lambda: server_recv(tcpcliesock), daemon=True)
-    server_recv_thread.start()
-    #Thread.start(server_recv(tcpcliesock))
-    data = {}
-    while 1:
-        while 1:
-            key = input(">>")
-            if key == "": break
-            value = input(">>")
-            if value == "": value = 0
-            data[key] = value
-        tcpcliesock.send(json.dumps(data).encode('utf-8'))
+finally:
+    window = QtWidgets.QMainWindow()
+    login_window = loginUI.UI_login()
+    login_window.setupUi(window)
+    window.show()
 
-tcpcliesock.close()
+    login_window.start_game_btn.clicked.connect(lambda: start_game(login_window.name_entry.text()))
+    login_window.join_game_btn.clicked.connect(lambda: join_game(login_window.name_entry.text(), login_window.game_id_entry.text()))
+    # server_recv_thread = Thread(target=lambda: server_recv(tcpcliesock), daemon=True)
+    # server_recv_thread.start()
+
+    # data = {}
+    # while 1:
+    #     while 1:
+    #         key = input(">> ")
+    #         if key == "": break
+    #         value = input(">> ")
+    #         if value.isdigit(): value = eval(value)
+    #         data[key] = value
+    #     tcpcliesock.send(json.dumps(data).encode('utf-8'))
+
+while not app.exec_():
+    # tcpcliesock.send(json.dumps({"type": "logout"}).encode('utf-8'))
+    tcpcliesock.close()
+    sys.exit()
+
