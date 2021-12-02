1ca0f013a4d09b81fac938cfe90c5fae399d5260
Maya1234567890 <vaksin.maya@gmail.com>
2021-12-02 18:42:25 +0200

    strategy & UI update
    added to the strategy and changed the testing:
    checking one move of the opponent and blocking his moves - +1 turn/chance to steal
    Also added and changed label names in the board UI

--- client.py
+++ client.py
@@ -5,9 +5,11 @@ from threading import Thread
 import loginUI
 import boardUi
 from PyQt5 import QtCore, QtGui, QtWidgets
+from random import randint
 import time
 
 popup = []
+
 
 def strategy(board):
     # if any turn can make you another turn
@@ -17,57 +19,93 @@ def strategy(board):
         while turn < 0:
             if turn < -6: turn = turn - 1
             turn = turn + 14
-        print(turn)
+        print(turn)  # the place where the last stone fell on the list board
+        # another turn
         if turn == 0:
             move(i)
             return
+        # a chance to steal
         if 0 < turn < 7 and board[turn] == 0:
             move(i)
             return
+        # if I reach the enemy's side
+        if hole > i:
+            # checking the opponent's turns
+            for j, op_hole in enumerate(board[8:]):
+                if op_hole == 0: continue
+                j = j + 8
+                print(j, op_hole)
+                op_turn = j - op_hole  # the place of the last stone fell
+                while op_turn < 0:
+                    if op_turn < -6: op_turn = op_turn - 1
+                    op_turn = op_turn + 14
+                print(op_turn)  # the place where the last stone fell on the list board
+                # the enemy has a chance to steal from me but i can defend
+                if 8 <= op_turn < 14 and board[op_turn] == 0 and (turn <= op_turn or turn <= j):
+                    move(i)
+                    return
+                if op_turn == 7 and i <= j:
+                    move(i)
+                    return
+    # temp
+    move(randint(1, 6))
+    return
 
-    print(board)
+
+    # print(board)
 
 
 def move(index):
     global server
     # Make random move
-    print(f"--Making move {index}--")
+    print(f"--Making a move from pit number {index}--")
 
     # Send Game Move message to server
     server.send(
         json.dumps({
             "type": "Game Move",
             "index": index
-        }).encode()
+        }).encode('utf-8')
     )
+    # temp
+    time.sleep(2)
 
+def update_board(board, ui):
+    holes = ui.get_all_holes()
+    for i, hole in enumerate(holes):
+        hole.setText(str(board[i]))
 
-def server_recv():
+def server_recv(ui):
     global server
     while True:
         try:
             msg_len = eval(server.recv(5).decode().strip("0"))
-            msg = server.recv(msg_len).decode()
-            # print(msg)
+            # print(msg_len, 1)
+            msg = server.recv(msg_len).decode('utf-8')
+            # print(msg, 2)
             data = json.loads(msg)
-            # msg = socket.recv(BUFSIZ).decode('utf-8')
             print(data)
+
             if data["type"] == "Board Update":
-                if data["your turn"]:
-                    strategy(data['board'])
-                    move(server)
-                # print(data["board"])
+                # make a function to change the ui accordingly
+                update_board(data['board'], ui)
+                ui.turn_lbl.setText("Your Turn: " + str(data["your turn"]))
+                if data["your turn"]: strategy(data['board'])
+
+            elif data["type"] == "Notification":
+                ui.opponent_lbl.setText("Game Opponent: " + data["data"][0:data["data"].index(" ")])
+            elif data["type"] == "Success":
+                ui.opponent_lbl.setText("Game Opponent: " + data["data"][data["data"].rfind(" "):])
 
                 # If we got an error
             elif data["type"] == "Error":
+                print(data)
 
-                if data["errtype"] == "Invalid Name":
-                    print(data)
+                """if data["errtype"] == "Invalid Name":
+                    print(data)"""
 
                 if data["errtype"] == "Invalid Move":
-                    print(data)
-                    move(server)
-
+                    strategy(data['board'])
                 # Game over message (includes log)
             elif data["type"] == "Game Over":
                 print(data)
@@ -83,62 +121,46 @@ def board_window(id):
     Form.show()
     global popup
     popup.append(Form)
-    print("Game ID"+str(id))
 
-    server_recv_thread = Thread(target=lambda: server_recv(), daemon=True)
+    server_recv_thread = Thread(target=lambda x=ui: server_recv(x), daemon=True)
     server_recv_thread.start()
 
 
 def start_game(name):
     global server
+
     if not name.isalnum():
         print("Enter a name, not a digit")
         return
-    server.send(json.dumps({"type":"Login","name":name}).encode('utf-8'))
+    server.send(json.dumps({"type": "Login", "name": name}).encode('utf-8'))
     msg = json.loads(server.recv(BUFSIZ).decode()[5:])
     print(msg)
-    if msg["type"] != 'Welcome':
-        print(msg)
-        return
+    if msg["type"] != 'Login Successfull': return
+
     server.send(json.dumps({"type": "Start Game"}).encode('utf-8'))
     msg = json.loads(server.recv(BUFSIZ).decode()[5:])
-    if msg['type'] != 'Login Successfull':
-        print(msg)
-        return
-    msg = json.loads(server.recv(BUFSIZ).decode()[5:])
-    if msg['type'] != 'Success':
-        print(msg)
-        return
+    print(msg)
+    if msg['type'] != 'Success': return
 
     game_id = msg['game_id']
     board_window(game_id)
 
-    # trying to open the board window, however failing :(
-    print(":P")
-    # Form = QtWidgets.QWidget()
-    # ui = boardUi.Ui_Form()
-    # ui.setupUi(Form)
-    # Form.show()
-    #
-    # global popup
-    # popup.append(Form)
-
 
 def join_game(name, id):
     global server
+
     if not name.isalnum() or not id.isdigit():
         print("Name can't be a digit and the game ID has to be a digit")
         return
-    server.send(json.dumps({"type":"Login","name":name}).encode('utf-8'))
 
+    server.send(json.dumps({"type": "Login", "name": name}).encode('utf-8'))
     msg = json.loads(server.recv(BUFSIZ).decode()[5:])
     print(msg)
-    if msg["type"] != 'Welcome':
-        print(msg)
-        return
-    server.send(json.dumps({"type": "Join Game", "game_id":eval(id)}).encode('utf-8'))
-    msg = json.loads(server.recv(BUFSIZ).decode()[5:])
-    print(msg)
+    if msg["type"] != 'Login Successfull': return
+
+    server.send(json.dumps({"type": "Join Game", "game_id": eval(id)}).encode('utf-8'))
+    """msg = json.loads(server.recv(BUFSIZ).decode()[5:])
+    print(msg, "!!!")"""
     board_window(id)
 
 
@@ -158,6 +180,8 @@ except TimeoutError:
     print("Time ran out. Please try again later")
 finally:
     server.settimeout(None)
+    msg = json.loads(server.recv(BUFSIZ).decode()[5:])
+    print(msg)
 
     window = QtWidgets.QMainWindow()
     login_window = loginUI.UI_login()
@@ -165,20 +189,20 @@ finally:
     window.show()
 
     login_window.start_game_btn.clicked.connect(lambda: start_game(login_window.name_entry.text()))
-    login_window.join_game_btn.clicked.connect(lambda: join_game(login_window.name_entry.text(), login_window.game_id_entry.text()))
+    login_window.join_game_btn.clicked.connect(
+        lambda: join_game(login_window.name_entry.text(), login_window.game_id_entry.text()))
 
 while not app.exec_():
     server.send(json.dumps({"type": "Logout"}).encode('utf-8'))
     server.close()
     sys.exit()
 
-
-# get second turn
+# get second turn ✓
 # protect or attack
 # limit the number of your stones and if it gets to the maximum get rid of them
-# try to get an empty hole
-# check if the other player can eat your stones == check all possible outcomes
-# check if he can mess up your next turn if no go for it!
-# check if you can mess his turn if you can go for it!
+# try to get an empty hole ✓
+# check if the other player can eat your stones ✓ => check all possible outcomes /
+# check if he can mess up your next turn if no go for it! ✓
+# check if you can mess his turn if you can go for it! ✓
 # try to get rid off of the greatest heap at the end of your board if they are equal get rid off the closest to you stones.
 # try to get rid off of the smallest heap at the start of your board
--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-board = [2, 4, 0, 4, 5, 4, 5, 0, 4, 4, 0, 4, 4, 3]
+board = [0, 3, 1, 1, 1, 1, 1, 0, 1, 4, 0, 4, 2, 6]
 # boar= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 def strategy(board):
@@ -17,18 +17,24 @@ def strategy(board):
         if 0<turn<7 and board[turn] == 0:
             print("hole",i)
             return
-        if board[14-i] == 0:
-            for j, op_hole in enumerate(board[8:14]):
-                if j == 0 or hole == 0: continue
-                j = j + 7
-                op_turn = j - op_hole  # te place of the last stone fell
-                while op_turn < 0:
-                    if op_turn < -6: op_turn = op_turn + 1
-                    op_turn = op_turn+14
-                print("sof tor yariv", op_turn)
-                if op_turn == 0:
-                    print("hole", j)
-                if 8 < op_turn < 14 and board[op_turn] == 0:
-                    print("hole", j)
-                    return
+
+        #ליריב יש הזדמנות לגנוב ממני כדורים
+        for j, op_hole in enumerate(board[8:]):
+            if op_hole == 0: continue
+            j = j + 8
+            print(j, op_hole)
+            op_turn = j - op_hole  # te place of the last stone fell
+            while op_turn < 0:
+                if op_turn < -6: op_turn = op_turn - 1
+                op_turn = op_turn+14
+            print("sof tor yariv", op_turn)
+
+            """if op_turn == 0:
+                print("hole", j)"""
+            if 8 <= op_turn < 14 and board[op_turn] == 0 and hole > i and (turn <= op_turn or turn <= j):
+                print("hole", i)
+                return
+            if op_turn == 7 and (turn <= j):
+                print("hole", i)
+                return
 strategy(board)
